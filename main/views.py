2e43abcee5871b1ab63e551513fff045adf1f964
Harshita Gaur <harshita.gaur1007@gmail.com>
2025-03-03 15:51:02 +0530

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,redirect
+from .models import Project, ContactMessage, Skill
+from .forms import ContactForm
+
+def portfolio(request):
+    projects = Project.objects.all()
+    skills = Skill.objects.all() 
+    form = ContactForm()
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('portfolio')  
+
+    return render(request, 'main/portfolio.html', {
+        'projects': projects,
+        'skills': skills,
+        'form': form
+    })
